99d8fd0aab4139be3a01a8d5481366585331a842
LeviAckerman-ai <levi1scoutregiment@gmail.com>
2026-08-09 22:02:44 +0530

    setup scripts for vercel deployment

--- expo-app/src/env.ts
+++ expo-app/src/env.ts
@@ -1,6 +1,3 @@
-import { Platform } from 'react-native';
-
 export const env = {
-  EXPO_PUBLIC_SERVER_URL:
-    Platform.OS === 'web' ? process.env.EXPO_PUBLIC_WEB_URL! : process.env.EXPO_PUBLIC_SERVER_URL!,
+  EXPO_PUBLIC_SERVER_URL: process.env.EXPO_PUBLIC_SERVER_URL!,
 };
